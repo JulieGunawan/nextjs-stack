09f1792745e1ad6993339587d3e947ad8504f2b3
JulieGunawan <48563515+JulieGunawan@users.noreply.github.com>
2023-11-16 18:16:06 -0700

    test the post api for user signup

--- auth-app/src/app/signup/page.tsx
+++ auth-app/src/app/signup/page.tsx
@@ -2,8 +2,8 @@
 import Link from "next/link";
 import React, {useEffect} from "react";
 import { useRouter } from "next/navigation";
-import { axios } from "axios";
-
+import axios from "axios";
+import {toast} from "react-hot-toast";
 export default function SignupPage() {
     //once the user sign up, it will be redirected to login page, using router to do that
     const router = useRouter();
@@ -15,7 +15,20 @@ export default function SignupPage() {
         });
     //setting to false will not make the button disabled
     const [buttonDisabled, setButtonDisabled] = React.useState(false);
+    const [loading, setLoading] = React.useState(false);
     const onSignup = async () => {    
+        try{
+            setLoading(true);
+            const response = await axios.post("/api/users/signup", user);
+            console.log("Signup success", response.data);
+            //once user succesfully signup, redirect to login page
+            router.push("/login");
+        } catch (error: any){
+            console.log("Sign up failed", error.message);
+            toast.error(error.message);
+        } finally {
+            setLoading(false);
+        }
     }
      
     useEffect(() => {
@@ -28,7 +41,7 @@ export default function SignupPage() {
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2 text-center bg-black">
-            <h1 className="text-white text-2xl">Signup Page</h1>
+            <h1 className="text-white text-2xl">{loading ? "Processing..." : "Sign Up"}</h1>
             <hr/>
             <label htmlFor="username" className="text-white text-2xl">Username</label>
             <input 
